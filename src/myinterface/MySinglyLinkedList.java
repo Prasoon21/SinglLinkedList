427f4c3bc25f8aaed9d4bb485f8da6ca2e3de52b
DELL <prashun.parashar_cs19@gla.ac.in>
2021-02-10 10:59:58 +0530

    MySinglyLinkedList

---
+++ src/myinterface/MySinglyLinkedList.java
@@ -0,0 +1,24 @@
+package myinterface;
+
+public interface MySinglyLinkedList {
+    // insert at beginning
+    //insert at head
+    //add first
+    void addFirst(int element);
+    //insertAtEnd
+    //insertAtTail
+    //void addLast(int element);
+    boolean isEmpty();// will give true if linked list is empty
+    int size();// will give total no. of elements in linked list
+    void addLast(int element);
+    void addLastWithoutUsingTail(int element);
+    void traverse();
+    void addAfterGivenNode(int givenElement, int element);
+    // void addBeforeGivenNode(int givenElement, int element);
+    int removeFirst();
+    int removeLast();
+    int removeGivenNode(int givenElement);
+    boolean search(int element);
+
+
+}
